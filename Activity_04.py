a83485f51ca25023f372c6c0c3801c60f5cd6fc5
rachana1010 <89650498+rachana1010@users.noreply.github.com>
2021-08-28 15:55:27 +0530

    Update Activity_04.py

--- Activity_04.py
+++ Activity_04.py
@@ -1,4 +1,7 @@
 x=int(input("Enter the first number: "))
 y=int(input("Enter the second number: "))
 s=x+y
-print("%d + %d = %d"%(x,y,s))
+print("%d+%d=%d"%(x,y,s))
+print(f'{x}+{y}={s}')
+print('{}+{}={}'.format(x,y,s))
+print(repr(x)+"+"+repr(y)+"="+repr(s))
