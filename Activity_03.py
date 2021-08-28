26ee1be3dd95ccb12aa455bff26b9a55186fa779
rachana1010 <89650498+rachana1010@users.noreply.github.com>
2021-08-28 14:00:04 +0530

    Update Activity_03.py

--- Activity_03.py
+++ Activity_03.py
@@ -1,6 +1,5 @@
 str1=input("Enter the first string: ")
 str2=input("Enter the second string: ")
 con_str=str1+str2
-print(con_str)
 rep_str=5*con_str
-print(rep_str)
+print("%s \n%s"%(con_str,rep_str))
